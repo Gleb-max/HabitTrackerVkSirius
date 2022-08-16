391ecfa5d88397a4d8d1a3a464bcbef4f7061bb6
gleb <gleb.demin.04@mail.ru>
2022-08-16 10:52:48 +0300

    feat:  - сделал ресайкл для списка заявок   - немного переделал карточку заявки

---
+++ app/src/main/java/com/habit/tracker/presentation/view/adapter/RequestItemViewHolder.kt
@@ -0,0 +1,14 @@
+package com.habit.tracker.presentation.view.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.habit.tracker.databinding.LayoutRequestCardBinding
+import com.habit.tracker.domain.entity.Request
+
+class RequestItemViewHolder(
+    val binding: LayoutRequestCardBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(todoItem: Request) {
+//todo
+    }
+}
