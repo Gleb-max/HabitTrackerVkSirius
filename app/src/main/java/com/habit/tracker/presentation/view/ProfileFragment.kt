5cd5e11551a6113a782ae885c4dcbe02f7c2eb27
city2 <bariy.litievich@gmail.com>
2022-08-16 11:30:47 +0300

    Сверстал BottomSheet для фильтров, весь код для него закомменчен в файле ProfileFragment

--- app/src/main/java/com/habit/tracker/presentation/view/ProfileFragment.kt
+++ app/src/main/java/com/habit/tracker/presentation/view/ProfileFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.habit.tracker.R
 import com.habit.tracker.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -19,5 +23,46 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    // это BottomSheet, надо прикрутить это к карте
+        // и кнопку тоже переопределить
+//        val buttonSheet = binding.buttonSheet
+//        buttonSheet.setOnClickListener {
+//
+//            // отображаем BottomSheet
+//            val view: View = layoutInflater.inflate(R.layout.item_bottom_sheet, null)
+//            val dialog = BottomSheetDialog(requireContext())
+//            dialog.setContentView(view)
+//            dialog.show()
+//
+//            // связываем SeekBar с его TextView
+//            val seekBar = view.findViewById<SeekBar>(R.id.seek_bar_filter)
+//            val seekNum = view.findViewById<TextView>(R.id.seek_num)
+//
+//            var startPoint = 0
+//            var endPoint = 100
+//
+//            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+//                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+//                    seekNum.text = p1.toString()
+//                }
+//
+//                override fun onStartTrackingTouch(p0: SeekBar?) {
+//                    if (seekBar != null) {
+//                        startPoint = seekBar.progress
+//                    }
+//                }
+//
+//                override fun onStopTrackingTouch(p0: SeekBar?) {
+//                    if (seekBar != null) {
+//                        endPoint = seekBar.progress
+//                    }
+//                }
+//
+//            })
+//        }
     }
 }
