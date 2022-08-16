942ec2853bcc0bb24cf3ba1237ab5dc8e7d4fc6b
gleb <gleb.demin.04@mail.ru>
2022-08-16 10:19:59 +0300

    feat:  - дописал юзкейс для получения организаций  - добавил отображение моковых маркеров на карте

--- app/src/main/java/com/habit/tracker/presentation/view/MapFragment.kt
+++ app/src/main/java/com/habit/tracker/presentation/view/MapFragment.kt
@@ -1,24 +1,38 @@
 package com.habit.tracker.presentation.view
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.habit.tracker.R
+import com.habit.tracker.TrackerApp
 import com.habit.tracker.core.LocationPermissionHelper
 import com.habit.tracker.databinding.FragmentMapBinding
+import com.habit.tracker.domain.entity.Organization
+import com.habit.tracker.presentation.stateholder.MapViewModel
+import com.habit.tracker.presentation.stateholder.ViewModelFactory
 import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
 import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.OnPointAnnotationClickListener
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 import com.mapbox.maps.plugin.gestures.OnMoveListener
 import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
 import java.lang.ref.WeakReference
+import javax.inject.Inject
 
 class MapFragment : Fragment() {
 
@@ -26,18 +40,11 @@ class MapFragment : Fragment() {
     private val binding: FragmentMapBinding
         get() = _binding ?: throw RuntimeException("FragmentTaskListBinding == null")
 
-    private lateinit var locationPermissionHelper: LocationPermissionHelper
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMapBinding.inflate(inflater, container, false)
-        locationPermissionHelper = LocationPermissionHelper(WeakReference(requireActivity()))
-        locationPermissionHelper.checkPermissions {
-            onMapReady()
-        }
-        return binding.root
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var viewModel: MapViewModel
+    private val component by lazy {
+        (requireActivity().application as TrackerApp).component
     }
 
     private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
@@ -47,6 +54,36 @@ class MapFragment : Fragment() {
     private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
         binding.mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
         binding.mapView.gestures.focalPoint = binding.mapView.getMapboxMap().pixelForCoordinate(it)
+    }
+
+    private lateinit var locationPermissionHelper: LocationPermissionHelper
+
+    override fun onAttach(context: Context) {
+        component.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MapViewModel::class.java]
+        locationPermissionHelper = LocationPermissionHelper(WeakReference(requireActivity()))
+        locationPermissionHelper.checkPermissions {
+            onMapReady()
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.organizations.observe(viewLifecycleOwner) {
+            for (org in it) addOrganizationMarkerToMap(org)
+        }
     }
 
     private val onMoveListener = object : OnMoveListener {
@@ -75,7 +112,32 @@ class MapFragment : Fragment() {
             ) {
                 initLocationComponent()
                 setupGesturesListener()
+                observeViewModel()
             }
+        }
+    }
+
+    private fun addOrganizationMarkerToMap(organization: Organization) {
+        val geo = organization.geo
+        activity?.let {
+            AppCompatResources.getDrawable(it, R.drawable.ic_profile)?.toBitmap(100, 100)
+                ?.let { b ->
+                    val annotationApi = binding.mapView.annotations
+                    val pointAnnotationManager =
+                        annotationApi.createPointAnnotationManager(binding.mapView)
+                    pointAnnotationManager.addClickListener(OnPointAnnotationClickListener {
+                        Log.e(
+                            "organ",
+                            organization.toString()
+                        )
+                        true
+                    })
+                    val pointAnnotationOptions: PointAnnotationOptions =
+                        PointAnnotationOptions()
+                            .withPoint(Point.fromLngLat(geo.long, geo.lat))
+                            .withIconImage(b)
+                    pointAnnotationManager.create(pointAnnotationOptions)
+                }
         }
     }
 
