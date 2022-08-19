fd2bc3c9fd8b36c092c022766ef4d703d1c5a8fc
city2 <bariy.litievich@gmail.com>
2022-08-19 23:12:31 +0300

    Приделал на кнопку фильтров bottom sheet с фильтрами, поправил форму регистрации, добавил закрытие клавиатуры на вводе кода из смс, поменял иконку организации

--- app/src/main/java/com/habit/tracker/presentation/generics/GenericTextWatcher.kt
+++ app/src/main/java/com/habit/tracker/presentation/generics/GenericTextWatcher.kt
@@ -1,8 +1,11 @@
 package com.habit.tracker.presentation.generics
 
+import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import com.habit.tracker.R
 
 class GenericTextWatcher internal constructor(private val currentView: View, private val nextView: View?) :
@@ -13,6 +16,10 @@ class GenericTextWatcher internal constructor(private val currentView: View, pri
             R.id.editTextNumber -> if (text.isNotEmpty()) nextView!!.requestFocus()
             R.id.editTextNumber2 -> if (text.isNotEmpty()) nextView!!.requestFocus()
             R.id.editTextNumber3 -> if (text.isNotEmpty()) nextView!!.requestFocus()
+            R.id.editTextNumber4 -> if (text.isNotEmpty()) {
+                val imm = currentView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(currentView.windowToken, 0)
+            }
             //You can use EditText4 same as above to hide the keyboard
         }
     }
